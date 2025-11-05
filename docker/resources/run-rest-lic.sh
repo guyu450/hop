6eeca813965403288979a661c6e91da2ad1ee48b
zhanglijuan <zhanglijuan@souche.com>
2025-11-05 15:21:02 +0800

    feat: 帮助文档

---
+++ docker/resources/run-rest-lic.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# 判断当前用户是root，则退出程序，否则执行
+echo "run-web-lic.sh start"
+# 判断当前用户是否是root
+if [ "$(id -u)" -eq 0 ]; then
+    echo "Error: 禁止以root用户运行容器" >&2
+    exit 1
+else
+  /tmp/check_license.sh
+  # 如果检查到没有授权，则退出程序
+  if [ $? -eq 1 ]; then
+    echo "Error: 授权失败" >&2
+    exit 1
+  else
+    echo "授权成功，启动主程序"
+    # 再执行主程序
+    exec /bin/bash /tmp/run-rest.sh "$@"
+  fi
+fi
